adfcf75482f68dd226a3906d022cc7f0dd4e2834
Mario <Mario@PLANETA_99>
2024-11-29 19:26:42 -0500

    Se ccrea la base del aplicativo

---
+++ Prueba Repositorios/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Repositorios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
