227e58783acaa4a28f023b99a6f216178889083a
Mario <Mario@PLANETA_99>
2024-12-06 15:08:00 -0500

    por siaca

---
+++ ProyectoFinal/Controllers/PersonasController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal.Context;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Controllers
+{
+    public class PersonasController : Controller
+    {
+        private ProyectoFinalContext db = new ProyectoFinalContext();
+
+        // GET: Personas
+        public ActionResult Index()
+        {
+            return View(db.Personas.ToList());
+        }
+
+        // GET: Personas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // GET: Personas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombres,Apellidos,NumeroIdentificacion,Email,TipoIdentificacion,FechaCreacion")] Personas personas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Personas.Add(personas);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(personas);
+        }
+
+        // GET: Personas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // POST: Personas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombres,Apellidos,NumeroIdentificacion,Email,TipoIdentificacion,FechaCreacion")] Personas personas)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(personas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(personas);
+        }
+
+        // GET: Personas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Personas personas = db.Personas.Find(id);
+            if (personas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(personas);
+        }
+
+        // POST: Personas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Personas personas = db.Personas.Find(id);
+            db.Personas.Remove(personas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Solonacional/context/Solonacional.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Solonacional.Models;
+
+namespace Solonacional.context
+{
+    public class Solonacional : DbContext
+    {
+            public Solonacional() : base("name=conexion")
+            {
+            }
+
+            public DbSet<Usuarios> Usuarios { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Usuarios>()
+                    .ToTable("Usuarios")
+                    .HasKey(p => p.Identificador);
+
+                modelBuilder.Entity<Usuarios>()
+                    .Property(p => p.Identificador)
+                    .IsRequired()
+                    .HasColumnName("Identificador");
+
+                modelBuilder.Entity<Usuarios>()
+                    .Property(p => p.Usuario)
+                    .IsRequired()
+                    .HasColumnName("Usuario");
+
+                modelBuilder.Entity<Usuarios>()
+                    .Property(p => p.Pass)
+                    .IsRequired()
+                    .HasColumnName("Pass")
+                    .HasMaxLength(50);
+
+
+                modelBuilder.Entity<Usuarios>()
+                    .HasRequired(u => u.Personas)         // Un Usuario requiere una Persona
+                    .WithMany()                           // Una Persona puede tener muchos Usuarios (si aplica, o usa WithOne() si es 1:1)
+                    .HasForeignKey(u => u.PersonasID);    // La clave foránea es PersonasID
+
+
+            base.OnModelCreating(modelBuilder);
+            }
+        
+    }
+}
